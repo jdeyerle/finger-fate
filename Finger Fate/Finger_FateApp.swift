3fd53739ee4db03962b8724a7ae023ee3eb78ba1
jdeyerle <jdeyerle.dev@pm.me>
2026-07-11 23:24:03 -0700

    Initial Commit

---
+++ Finger Fate/Finger_FateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finger_FateApp.swift
+//  Finger Fate
+//
+//  Created by James Deyerle on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Finger_FateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finger FateTests/Finger_FateTests.swift
@@ -0,0 +1,19 @@
+//
+//  Finger_FateTests.swift
+//  Finger FateTests
+//
+//  Created by James Deyerle on 7/11/26.
+//
+
+import Testing
+@testable import Finger_Fate
+
+struct Finger_FateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
